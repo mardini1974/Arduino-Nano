0f9403ed22a3d9fa53221f6223962deaa5feaa95
mardini1974 <masounmardini@gmail.com>
2016-03-25 23:19:21 +0100

    Added files via upload

---
+++ arduinopendant3.ino
@@ -0,0 +1,38 @@
+
+#include <SoftwareSerial.h>
+
+
+
+
+
+SoftwareSerial JYmcu(3,2); 
+void setup() 
+{
+    Serial.begin(115200);
+    JYmcu.begin (115200);
+    
+    
+    Serial.write ('E');
+ 
+}
+
+void loop() 
+{
+;
+char ch, ky;
+        
+    if(Serial.available() >= 1)
+        {
+        char ch = Serial.read();
+        JYmcu.print(ch);
+        }               
+    if(JYmcu.available() >= 1)
+        {
+      char ky=JYmcu.read();
+        Serial.print (ky);
+  
+       }    
+}
+
+
+
